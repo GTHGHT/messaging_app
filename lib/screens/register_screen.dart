1322e97385f70ac1a2a898bbeff9a2cddf58fb64
GTHGHT <gilang.example@gmail.com>
2022-05-17 21:51:01 +0800

    adding support for android 12

---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreensState();
+}
+
+class _RegisterScreensState extends State<RegisterScreen> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+  int passwordCounter = 0;
+
+  @override
+  void dispose() {
+    usernameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Desain Ulang Bagian Register
+    return Scaffold(
+      body: SafeArea(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints.expand(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: (MediaQuery.of(context).size.width / 6) * 5,
+                child: TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  controller: usernameController,
+                  decoration: InputDecoration(hintText: "Username"),
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              SizedBox(
+                width: (MediaQuery.of(context).size.width / 6) * 5,
+                child: TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(hintText: "Password"),
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  buildCounter: (
+                    BuildContext context, {
+                    required int currentLength,
+                    required int? maxLength,
+                    required bool isFocused,
+                  }) {
+                    return Text(
+                      '$currentLength Karakter',
+                    );
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 2,
+                child: ElevatedButton(
+                  onPressed: null,
+                  child: Text("Login"),
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 2,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("Kembali"),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
